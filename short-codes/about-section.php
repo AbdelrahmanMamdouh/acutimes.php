916c9fdea66dccb41d2cbabf9265748af6b7daaa
Marc Wafik <marc.wafik@gmail.com>
2017-01-25 20:57:03 +0200

    short code about section img is now circular

--- short-codes/about-section.php
+++ short-codes/about-section.php
@@ -6,10 +6,10 @@ function cjc_shortcode_aboutus_section( $atts, $content = null ) {
 		'direction'	=>'Right'
 	 ), $atts );
 
-	$img = $a['img']? wp_get_attachment_image($a['img'] ):'';
+	$img = $a['img']? wp_get_attachment_url($a['img'] ):'';
 
 	$img_css_class = $text_css_class = '';
-	
+
 	if($a['direction']=='Right'){
 		$img_css_class = ' col-md-push-8 ';
 		$text_css_class = ' col-md-pull-4 ';
@@ -19,17 +19,20 @@ function cjc_shortcode_aboutus_section( $atts, $content = null ) {
 	?>
 	<div class="c-section--tall">
 		<div class="row">
+		
 			<div class="col-md-4 <?php echo $img_css_class ?>">
-				<div class="circule circule--xl circule--center">
-					<div class="circule__content">
-						<?php echo $img ?>
+				<div class="circle circle--xl circle--center">
+					<div class="circle__content">
+						<img style="height: 255px;" src="<?php echo $img ?>" alt="">
 					</div>
 				</div>
 			</div>
+
 			<div class="col-md-8 <?php echo $text_css_class ?>">
 				<h1><?php echo $a['title'].$a['direction'] ?></h1>
 				<?php echo $content ?>
 			</div>
+
 		</div>
 	</div>
 	<?php
