dd3dd75fd3c8bf2f07aa5329bff1118f2decb7c6
Marc Wafik <marc.wafik@gmail.com>
2017-01-27 15:20:21 +0200

    fixed main page html

--- page.php
+++ page.php
@@ -13,28 +13,28 @@
  */
 
 get_header(); ?>
+<?php if( !is_home() &&  is_front_page()){ ?>
+	<main class="inner" id="primary">
+		<section>
+			<div class="container-fluid">
+ <?php } ?>	
+				<?php
+				while ( have_posts() ) : the_post();
 
-<main class="inner" id="primary" <?php if( !is_home() &&  is_front_page()){ ?> style="padding-top: 0"<?php } ?>>
-	<section>
-		<div class="container-fluid">
-			
-			<?php
-			while ( have_posts() ) : the_post();
+					get_template_part( 'template-parts/content', 'page' );
 
-				get_template_part( 'template-parts/content', 'page' );
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-			
-		</div>
-	</section>
-</main>
-
+				endwhile; // End of the loop.
+				?>
+<?php if( !is_home() &&  is_front_page()){ ?>		
+			</div>
+		</section>
+	</main>
+ <?php } ?>	
 <?php
 get_sidebar();
 get_footer();
