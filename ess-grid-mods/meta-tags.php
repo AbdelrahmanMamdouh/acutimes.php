8c9d26a8139b810fd99d036163a3316d03f1defa
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-01-27 21:12:09 +0200

    -  Added "genres_list" meta tag to ess. grid

--- ess-grid-mods/meta-tags.php
+++ ess-grid-mods/meta-tags.php
@@ -1,7 +1,5 @@
 <?php
 
-/* * ***************** "theme_dir" meta tag insertion ******************* */
-
 add_filter('essgrid_post_meta_handle', 'eg_add_post_option');
 add_filter('essgrid_post_meta_content', 'eg_add_post_content', 10, 4);
 
@@ -9,6 +7,9 @@ function eg_add_post_option($post_options) {
 
     // where 'theme_dir' is the attribute of the data to pull in
     $post_options['theme_dir'] = array('name' => 'Theme Directory');
+
+    // where 'genres_list' is the attribute of the data to pull in
+    $post_options['genres_list'] = array('name' => 'Genres List');
 
     return $post_options;
 }
@@ -22,8 +23,18 @@ function eg_add_post_content($meta_value, $meta, $post_id, $post) {
         return get_template_directory_uri();
     }
 
-    return $meta_value;
+    // where 'genres_list' is the attribute of the data to pull in
+    if ($meta === 'genres_list') {
+
+    	$genres = wp_get_post_terms( $post_id, 'genre');
+
+    	foreach ($genres as $genre)	{	
+    		$genres_names[] = $genre->name;
+    	}
+
+        return implode(", ", $genres_names);
+    }
+
 }
 
-/* * ***************** "theme_dir" meta tag insertion ******************* */
 ?>
