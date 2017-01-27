bfa9839b9d765c0d6e20f8fbad732fa3605c23e0
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-01-27 06:30:13 +0200

    - Deleted the page-home.php template page -  Added shortcode for the home page parallax slider -  Added parallax slider element into VC -  Added shortcode utilities to parse the content

--- short-codes/load-shortcodes.php
+++ short-codes/load-shortcodes.php
@@ -1,3 +1,5 @@
 <?php
+require_once get_template_directory() . '/short-codes/shortcodes-utilities.php';
 require_once get_template_directory() . '/short-codes/about-section.php';
 require_once get_template_directory() . '/short-codes/night.php';
+require_once get_template_directory() . '/short-codes/parallax-slider.php';
---
+++ short-codes/parallax-slider.php
@@ -0,0 +1,86 @@
+<?php
+function cjc_shortcode_parallax_slider( $atts, $content = null ) {
+	 $args = shortcode_atts( array(
+		'title' 	=> '',
+		'class' 	=> '',
+		'id' 	=> '',
+	 ), $atts );
+
+	$shortcodes = get_shortcodes( $content );
+	$content = strip_shortcodes( $content );
+
+	ob_start();// start buffer
+	?>
+	 <div class="container-fluid" id="trigger">
+        <div class="parallax <?php echo $args['class']; ?>" id="<?php echo $args['id']; ?>">
+            <div class="parallax__content">
+
+                <img src="<?php echo get_template_directory_uri();?>/img/events-top.png" alt="">
+
+                <div class="events-content">
+
+                    <h2 class="centered"> <?php echo $args['title']; ?> </h2>
+
+                    <?php echo do_shortcode( implode($shortcodes) ); ?>
+
+                    <?php echo $content ?>
+
+                </div>
+
+                <img src="<?php echo get_template_directory_uri();?>/img/events-bottom.png" alt="">
+
+            </div>
+        </div>
+    </div>
+	<?php
+	return  ob_get_clean();// return buffer
+}
+
+add_shortcode( 'cjc_parallax_slider' , 'cjc_shortcode_parallax_slider' );
+
+//check if visual composer is installed
+if(function_exists ('vc_map')){
+vc_map( array(
+	"name" 			=> __("CJC Parallax Slider"),
+	"base" 			=> "cjc_parallax_slider",
+	"category" 		=> __('Cairo Jazz Club'),
+	"params" => array(
+		array(
+			"type" 			=> "textfield",
+			"holder" 		=> "div",
+			"class" 		=> "",
+			"heading" 		=> __("Slider Name"),
+			"param_name"	=> "title",
+			"value" 		=> __(""),
+			"description" 	=> __("Enter text used as slider title (Note: located above content element).")
+		),
+		array(
+			"type" 			=> "textarea_html",
+			"holder" 		=> "div",
+			"class" 		=> "",
+			"heading" 		=> __( "Content" ),
+			"param_name" 	=> "content", // Important: Only one textarea_html param per content element allowed and it should have "content" as a "param_name"
+			"value" 		=> __(""),
+			"description" 	=> __( "Provide the content for this parallax slider." )
+		),
+		array(
+			"type" 			=> "textfield",
+			"holder" 		=> "div",
+			"class" 		=> "",
+			"heading" 		=> __("Extra class name"),
+			"param_name"	=> "class",
+			"value" 		=> __(""),
+			"description" 	=> __("Style particular content element differently - add a class name and refer to it in custom CSS.")
+		),
+		array(
+			"type" 			=> "textfield",
+			"holder" 		=> "div",
+			"class" 		=> "",
+			"heading" 		=> __("Extra id name"),
+			"param_name"	=> "id",
+			"value" 		=> __(""),
+			"description" 	=> __("Style particular content element differently - add a id name and refer to it in custom CSS.")
+		),
+	)
+));
+}
