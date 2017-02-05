1c021643b3a124d1a48b90c33e1ab1173e2c88bb
Abdelrahman Abdelhamed <AbdelrhmanAbdelhamed@gmail.com>
2017-02-05 05:07:34 +0200

    - LOAD the circle-aritst file

--- website/wp-content/themes/Cairo-Jazz-Club/short-codes/init.php
+++ website/wp-content/themes/Cairo-Jazz-Club/short-codes/init.php
@@ -6,3 +6,4 @@ require_once get_template_directory() . '/short-codes/about-section.php';
 require_once get_template_directory() . '/short-codes/night.php';
 require_once get_template_directory() . '/short-codes/parallax-slider.php';
 require_once get_template_directory() . '/short-codes/advertisement.php';
+require_once get_template_directory() . '/short-codes/circle-artist.php';
