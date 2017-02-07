b7144808c3171783b1ef34fed4916b02dcec3640
Marc Wafik <marc.wafik@gmail.com>
2017-02-07 16:48:35 +0200

    sperated the artist query from the event query

--- website/wp-content/themes/Cairo-Jazz-Club/template-parts/desktop-events.php
+++ website/wp-content/themes/Cairo-Jazz-Club/template-parts/desktop-events.php
@@ -1,50 +1,56 @@
+<?php 
+
+$args = array( 
+	'post_type' => 'avent',
+	'post_status' => 'publish',
+	'posts_per_page' => 7,
+	'meta_key'  => 'date',
+	'meta_value'   => current_time( "Ymd" ),
+	'meta_compare' => '>=',
+	'orderby' => 'meta_value_num',
+	'order' => 'ASC'
+);
+
+$the_query = new WP_Query( $args );
+
+$positions = array(
+	'circle--lg pos-1',
+	'circle--md pos-2',
+	'circle--md pos-3',
+	'circle--lg pos-4',
+	'circle--md pos-5',
+	'circle--md pos-6',
+	'circle--md pos-7'
+);
+
+$i = 0;
+?>
 <div class="desktop-events hidden-xs">
-
-    <?php 
-
-        $args = array( 
-            'post_type' => 'avent',
-            'post_status' => 'publish',
-            'posts_per_page' => 7,
-            'meta_key'  => 'date',
-            'meta_value'   => date( "Ymd" ),
-			'meta_compare' => '>=',
-            'orderby' => 'meta_value_num',
-            'order' => 'ASC'
-        );
-
-        $the_query = new WP_Query( $args );
-
-        $positions = array(
-            'circle--lg pos-1',
-            'circle--md pos-2',
-            'circle--md pos-3',
-            'circle--lg pos-4',
-            'circle--md pos-5',
-            'circle--md pos-6',
-            'circle--md pos-7'
-        );
-
-        $i = 0;
-
-    ?>
-
     <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
 
-            <div class="event circle <?php echo $positions[$i] ?>">
-
+            <div class="event circle <?php echo $positions[$i++] ?>">
                 <?php get_template_part('template-parts/event-circle'); ?>
-                <?php get_template_part('template-parts/event-artist-circle'); ?>
-
             </div>
-
-        <?php $i++; ?>
 
     <?php endwhile; else: ?>
 
         <p>Sorry, there are no events to display</p>
 
     <?php endif; ?>
-
-    <?php wp_reset_query(); ?>
 </div> <!-- /.desktop-events -->
+
+<?php
+wp_reset_query();
+$the_query = new WP_Query( $args );
+$i = 0;
+?>
+
+<div class="rand-artists hidden-xs">
+	<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();?>
+
+		<div class="artist circle circle--md pos-<?php echo $i ?>">
+			<?php get_template_part('template-parts/event-artist-circle'); ?>
+		</div>
+
+    <?php endwhile; endif; ?>
+</div> <!-- /.rand-artists -->
