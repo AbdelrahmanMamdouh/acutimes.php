00d4397b325170d73113c12a2cb9892491b1eca7
Marc Wafik <marc.wafik@gmail.com>
2017-02-08 23:42:41 +0200

    added FB img if user is loged in ( social icons )

--- website/wp-content/themes/Cairo-Jazz-Club/template-parts/social-icons.php
+++ website/wp-content/themes/Cairo-Jazz-Club/template-parts/social-icons.php
@@ -7,16 +7,22 @@
  * @package cjc
  */
 
-$login		= wp_login_url( get_permalink() ) . '&loginFacebook=1';
+global $init;
+$fbUser = $init->getUserDetails();
+
 $facebook	= get_theme_mod('cjc-social-media-facebook');
 $twitter	= get_theme_mod('cjc-social-media-twitter');
 $youtube	= get_theme_mod('cjc-social-media-youtube');
 ?>
 
 <ul class="social-icons">
-	<?php if($login) { ?>
+
+	<?php if( !$init->IsLogged() ) { ?>
 		<li><a class="account modal-link" href="<?php echo get_template_directory_uri()?>/modal-templates/login-modal.php">Account</a></li>
-	<?php } ?>
+	<?php } else { ?>
+		<li><a class="account-loged-in modal-link" href="<?php echo get_template_directory_uri()?>/modal-templates/login-modal.php" style = 'background-image: url(<?php echo $fbUser->user_picture ?>); '><?php echo $fbUser->user_name ?></a></li>
+	<?php }?>
+
 	<?php if($facebook) { ?>
 		<li><a class="facebook" target="_blank" href="<?php echo $facebook ?>">Facebook</a></li>
 	<?php } ?>
@@ -28,4 +34,5 @@ $youtube	= get_theme_mod('cjc-social-media-youtube');
 	<?php if($youtube) { ?>
 		<li><a class="youtube" target="_blank" href="<?php echo $youtube ?>">Youtube</a></li>
 	<?php } ?>
+
 </ul>
