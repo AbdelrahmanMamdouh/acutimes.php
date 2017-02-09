7c1626cfc5250a9bda01a5c6db6b9915e9f36e92
Marc Wafik <marc.wafik@gmail.com>
2017-02-09 12:26:01 +0200

    fixed login modal html / style

--- website/wp-content/themes/Cairo-Jazz-Club/modal-templates/login-modal.php
+++ website/wp-content/themes/Cairo-Jazz-Club/modal-templates/login-modal.php
@@ -1,10 +1,33 @@
 <?php
     require_once('../../../../wp-load.php');
 ?>
-<div class='mfp-modal' id="login-modal" style='max-width: 550px;'>
-        <div class="mfp-modal-content">
-        <h3 class="modal-login-header">FaceBook</h3>
-            <div>loging to face book</div>
-        <a class="btn btn-facebook" href="">login to facebook</a>
-        </div>
+<style>
+	.mfp-auto-cursor .mfp-content {
+		max-width: 340px;
+	}
+</style>
+<div class='mfp-modal' id="login-modal">
+
+	<div class="mfp-modal-content">
+
+		<?php if (has_custom_logo()): ?>
+			<div class="col-md-12">
+				<div style="padding:35px;">
+					<a href="<?php echo site_url()?>" class="logo__link">
+						<img style="width:100%" src="<?php echo Mcustomizer::getCustomLogoURL() ?>" alt="Cairo Jazz Club Logo">
+					</a>
+				</div>
+			</div>
+		<?php endif; ?>
+		<div>
+			<a class="btn btn-facebook" style="width:100%" href="">Sign in with facebook</a><br>
+		</div>
+		<div>
+			<small> 
+				<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
+				We don't post anything to facebook
+			</small>
+		</div>
+	</div>
+
 </div>
