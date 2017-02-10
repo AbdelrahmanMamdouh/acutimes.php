e8c04ab20411015a652b1f278619453abcb12635
Marc Wafik <marc.wafik@gmail.com>
2017-02-10 08:01:13 +0200

    added the main empty pages

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { ContactUsPage } from '../pages/contact-us/contact-us';
+import { CalendarPage } from '../pages/calendar/calendar';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    Page1,
-    Page2
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    Page1,
-    Page2
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations: [
+		MyApp,
+		HomePage,
+		AboutPage,
+		ContactUsPage,
+		CalendarPage
+	],
+	imports: [
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		HomePage,
+		AboutPage,
+		ContactUsPage,
+		CalendarPage
+	],
+	providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
