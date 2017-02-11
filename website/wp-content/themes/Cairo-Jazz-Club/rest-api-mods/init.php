b95c3ac804afb76990d3732562032b3ae626fdb0
Marc Wafik <marc.wafik@gmail.com>
2017-02-11 11:47:58 +0200

    added the event calendar to rest api

--- website/wp-content/themes/Cairo-Jazz-Club/rest-api-mods/init.php
+++ website/wp-content/themes/Cairo-Jazz-Club/rest-api-mods/init.php
@@ -1,4 +1,5 @@
 <?php
 
 require_once get_template_directory() . '/rest-api-mods/albums.php';
+require_once get_template_directory() . '/rest-api-mods/events.php';
 require_once get_template_directory() . '/rest-api-mods/shortcodes.php';
