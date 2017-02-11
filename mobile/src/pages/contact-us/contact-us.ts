6bfefcade82c9786994954555c726b73bd4c88e8
Marc Wafik <marc.wafik@gmail.com>
2017-02-11 21:28:45 +0200

    almost done with contact us

--- mobile/src/pages/contact-us/contact-us.ts
+++ mobile/src/pages/contact-us/contact-us.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { CustomizerService, Customizer } from '../../providers/customizer-service';
 
 /*
   Generated class for the ContactUs page.
@@ -9,11 +10,35 @@ import { NavController, NavParams } from 'ionic-angular';
 */
 @Component({
 	selector: 'page-contact-us',
-	templateUrl: 'contact-us.html'
+	templateUrl: 'contact-us.html',
+	providers: [CustomizerService]
 })
 export class ContactUsPage {
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) { }
+	public address: string;
+	public email: string;
+	public phone: string;
+
+	public error;
+
+	constructor(public navCtrl: NavController, public navParams: NavParams, customizerService: CustomizerService) {
+
+		customizerService.getAll().subscribe(
+			function (customizer: Customizer) {
+
+				this.address = customizer.identity.address;
+				this.email = customizer.identity.email;
+				this.phone = customizer.identity.phone;
+
+			}.bind(this),
+			function (err) {
+
+				this.error = err;
+				console.warn(err);
+
+			}.bind(this));
+
+	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad ContactUsPage');
