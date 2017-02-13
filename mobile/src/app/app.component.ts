18b7d9555257c8c83840f17cf837f6eaa7dd3912
Marc Wafik <marc.wafik@gmail.com>
2017-02-13 18:11:09 +0200

    changed mainpage added moment lib added development mod to config

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { CalendarPage } from '../pages/calendar/calendar';
 export class MyApp {
 	@ViewChild(Nav) nav: Nav;
 
-	rootPage: any = ContactUsPage; //= LandingPage;
+	rootPage: any = LandingPage;
 
 	pages: Array<{ title: string, component: any }>;
 
--- mobile/src/pages/landing/landing.ts
+++ mobile/src/pages/landing/landing.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
 import { Facebook } from 'ionic-native';
+
+import CONFIG from '../../app/config.json';
+import { HomePage } from '../home/home';
 
 @Component({
 	selector: 'page-landing',
@@ -10,30 +14,39 @@ export class LandingPage {
 	logo = "assets/img/logo.png";
 
 	slides = [
-		{
-			image: "assets/img/landing-slide-1.png"
-		},
-		{
-			image: "assets/img/landing-slide-1.png"
-		},
-		{
-			image: "assets/img/landing-slide-1.png"
-		}
+		{ image: "assets/img/landing-slide-1.png" },
+		{ image: "assets/img/landing-slide-1.png" },
+		{ image: "assets/img/landing-slide-1.png" }
 	];
 
-	onFBLoginClick = function () {
-		Facebook.login(['email']).then(data => {
-			this.faceBookToken = data.authResponse.accessToken;
-			this.faceBookUserId = data.authResponse.userID;
+	faceBookToken;
+	faceBookUserId;
 
-			this.onGetDetailsClick();
-		});
+	constructor(public navCtrl: NavController, public navParams: NavParams) { }
+
+	onFBLoginClick() {
+
+		if (CONFIG.DEV.SKIP_LOGIN) {
+
+			this.navCtrl.setRoot(HomePage);
+
+		} else {
+			Facebook.login(['email']).then(data => {
+				this.faceBookToken = data.authResponse.accessToken;
+				this.faceBookUserId = data.authResponse.userID;
+
+				this.onGetDetailsClick();
+			});
+		}
+
 	}
 
-	onGetDetailsClick = function () {
+	onGetDetailsClick() {
+
 		Facebook.api(this.faceBookUserId + "/?fields=name,email,birthday,gender", ['public_profile']).then(data => {
 			alert(JSON.stringify(data));
 		});
+
 	}
 
 }
