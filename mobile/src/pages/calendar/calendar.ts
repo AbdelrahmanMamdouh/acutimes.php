44a674a59aeb4ca96d0d890597e9effcd3e1f83e
Migz <moahmmed.magdy.ali.96@gmail.com>
2017-02-13 12:50:06 +0200

    add calender page in app

--- mobile/src/pages/calendar/calendar.ts
+++ mobile/src/pages/calendar/calendar.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as $ from "jquery";
+import 'slick-carousel/slick/slick';
+import * as moment from 'moment';
+
+import { EventsService } from '../../providers/events-service';
+
+import { Event } from '../../providers/events-service';
 
 /*
   Generated class for the Calendar page.
@@ -13,10 +20,45 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CalendarPage {
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) { }
+	events: Event[];
+	all: Array<string>=["January","February","March","April","May","June","July","August","September","October","November","December"];
+	now = moment().format('MMMM');
+
+	constructor(public navCtrl: NavController, public navParams: NavParams , private eventsService: EventsService){
+			var thisMonth = moment().format('MMMM'); //get the current month
+			eventsService.getEvents("all").subscribe(events => { //get the events from the api
+			this.events = events;
+			var i=0;
+			for (var event in this.events) {
+				this.events[i]['day']= moment(this.events[i]['startDate']).format('dddd');
+				this.events[i]['month']=moment(this.events[i]['startDate']).format('MMMM');
+				 this.events[i]['iday']=moment(this.events[i]['startDate']).format('DD');
+				 i++;
+			}
+			i=0;
+		})
+	}
+
+	doinitSlick = true; // initialize it to true for the first run
+
+
+	initSlick(): void {
+		$('.events-slider').slick({
+			arrows: false,
+			mobileFirst: true,
+			centerMode: true,
+			centerPadding: '70px'
+		});
+		this.doinitSlick = false; // set it to false until you need to trigger again
+	}
+
+
+
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad CalendarPage');
+
+		
 	}
 
 }
