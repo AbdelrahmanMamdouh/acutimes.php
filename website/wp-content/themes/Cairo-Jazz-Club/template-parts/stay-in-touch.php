a264fea29d5e754800308078962f15868a0abb37
Marc Wafik <marc.wafik@gmail.com>
2017-02-18 15:14:18 +0200

    finshed stay in touch footer widget

--- website/wp-content/themes/Cairo-Jazz-Club/template-parts/stay-in-touch.php
+++ website/wp-content/themes/Cairo-Jazz-Club/template-parts/stay-in-touch.php
@@ -6,57 +6,36 @@
  *
  * @package cjc
  */
-
+ global $FBR_User_data;
+$CJC_genres = get_terms( 'genre' );
 ?>
 
-<p>Integer vitae libero ac risus egestas placerat.</p>
+<!--<p>Integer vitae libero ac risus egestas placerat.</p>-->
 
 <form action="/">
 	<div class="form-group">
 		<div class="input input--reverse">
-			<label for="email">Type Your Email</label>
-			<input type="email" class="form-control" id="foote_email">
+			<!--<label for="email">Type Your Email</label>-->
+			<input type="email" class="form-control" id="foote_email" placeholder='Type Your Email' value='<?php echo $FBR_User_data['email'] ?>'>
 		</div>
 	</div>
 	<h3>Choose your genre</h3>
 	<div class="row">
-		<div class="col-md-4">
-			<p>
-				<input type="checkbox" id="test1">
-				<label for="test1">Avant-garde</label>
-			</p>
-			<p>
-				<input type="checkbox" id="test2" checked="checked">
-				<label for="test2">Blues</label>
-			</p>
-			<p>
-				<input type="checkbox" id="test3">
-				<label for="test3">Caribbean</label>
-			</p>
-			<p>
-				<input type="checkbox" id="test4">
-				<label for="test4">Country</label>
-			</p>
-		</div>
 
-		<div class="col-md-5">
-			<p>
-				<input type="checkbox" id="test5">
-				<label for="test5">R&amp;B and soul</label>
-			</p>
-			<p>
-				<input type="checkbox" id="test6">
-				<label for="test6">Rock</label>
-			</p>
-			<p>
-				<input type="checkbox" id="test7" checked="checked">
-				<label for="test7">Electronic</label>
-			</p>
-			<p>
-				<input type="checkbox" id="test8">
-				<label for="test8">Folk</label>
-			</p>
-		</div>
+		<?php foreach($CJC_genres as $gen) { ?>
+
+			<div class="col-md-4" style="overflow: hidden; max-height: 2.5rem; margin: 8px 0">
+				<p>
+					<input type="checkbox" 
+						id="<?php echo $gen->term_id ?>" 
+						<?php if(isset($FBR_User_data['genre_bol'][$gen->term_id])) echo 'checked' ?>
+					>
+					<label for="<?php echo $gen->term_id ?>" ><?php echo $gen->name ?></label>
+				</p>
+			</div>
+			
+		<?php } ?>
+
 	</div>
 
 	<submit class="btn btn-supporting btn-wide">Submit</submit>
