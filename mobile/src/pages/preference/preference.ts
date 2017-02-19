0994692ba52904ced10211a985deb3d569fd3a32
Marc Wafik <marc.wafik@gmail.com>
2017-02-20 01:09:27 +0200

    finshed prefs serice + controller

--- mobile/src/pages/preference/preference.ts
+++ mobile/src/pages/preference/preference.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { PreferenceService, Genre } from '../../providers/preference-service';
+import { FacebookService, User } from '../../providers/facebook-service';
 
 /*
   Generated class for the Preference page.
@@ -13,7 +15,30 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class PreferencePage {
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) { }
+	public genres: Genre[] = [];
+	public user_id: number;
+
+	constructor(public navCtrl: NavController, public navParams: NavParams, public prefService: PreferenceService, public fbService: FacebookService) {
+
+		fbService.getUser()
+			.then((user: User) => {
+				this.user_id = user.siteId;
+				prefService.get(this.user_id).subscribe(
+					(data: Genre[]) => {
+						this.genres = data;
+					}, (err) => console.warn(err)
+				);
+			}, (err) => console.warn(err));
+
+	}
+
+
+	onSubmit() {
+		if (!this.user_id) {
+			return;
+		}
+		this.prefService.send(this.user_id, this.genres);
+	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad PreferencePage');
