241f7c56e8179863a86668b411a181b22cdfbc31
Marc wafik <Marc wafik>
2017-02-24 14:04:52 +0200

    fixed a bug in user first login where if no user is logedin $firstLogin dosen't exist

--- website/wp-content/themes/Cairo-Jazz-Club/page.php
+++ website/wp-content/themes/Cairo-Jazz-Club/page.php
@@ -13,45 +13,37 @@
  */
 
 get_header(); 
- global $FBR_User_data;
-if( !is_home() &&  is_front_page() && $FBR_User_data['is_loged']){
+global $FBR_User_data;
 
-$firstLogin = count($FBR_User_data['genre_bol'])>0 ? 1:0;
-}
+$firstLogin = ( !is_home() &&  is_front_page() && $FBR_User_data['is_loged'] && count($FBR_User_data['genre_bol']) > 0 ) ? 1 : 0;
 ?>
 
-
-
-
 <input type=hidden id="filled" value="<?php echo $firstLogin ?>">
-
-	
 
 <?php if( !(!is_home() &&  is_front_page())){ ?>
 	<main class="inner" id="primary">
 		<section>
 			<div class="container-fluid">
+<?php } ?>
 
-	
-<?php } ?>	
 				<?php
-				while ( have_posts() ) : the_post();
+					while ( have_posts() ) : the_post();
 
-					get_template_part( 'template-parts/content', 'page' );
+						get_template_part( 'template-parts/content', 'page' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-				endwhile; // End of the loop.
+					endwhile; // End of the loop.
 				?>
+
 <?php if( !(!is_home() &&  is_front_page())){ ?>	
 			</div>
 		</section>
 	</main>
 <?php } ?>
-
 
 <section>
 	<div class="container-fluid">
