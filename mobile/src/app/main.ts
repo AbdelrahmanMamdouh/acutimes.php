8506d48244770aea7ec28707ef0782192f8129cc
Marc Wafik <marc.wafik@hotmail.com>
2017-03-03 22:58:46 +0200

    added release mode changed splashscreen time

--- mobile/src/app/main.ts
+++ mobile/src/app/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
+import CONFIG from './config.json';
 
+if (CONFIG.RELEASE) { enableProdMode(); }
 platformBrowserDynamic().bootstrapModule(AppModule);
